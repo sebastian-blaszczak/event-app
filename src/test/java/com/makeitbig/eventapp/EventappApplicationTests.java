e662d6c51574c29f60f5141ff8e659acae79fd5f
sebastian-blaszczak <sebastian.blaszczak.1994@gmail.com>
2019-09-07 10:57:08 +0200

    Initial comit

---
+++ src/test/java/com/makeitbig/eventapp/EventappApplicationTests.java
@@ -0,0 +1,16 @@
+package com.makeitbig.eventapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EventappApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
