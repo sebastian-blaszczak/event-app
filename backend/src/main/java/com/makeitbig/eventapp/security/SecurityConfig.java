f7c10924c0f8d74142a4d4bdf25f21c843d3cc40
sebastian-blaszczak <sebastian.blaszczak.1994@gmail.com>
2019-09-08 14:48:01 +0200

    Adding frontend prototype

--- backend/src/main/java/com/makeitbig/eventapp/security/SecurityConfig.java
+++ backend/src/main/java/com/makeitbig/eventapp/security/SecurityConfig.java
@@ -1,16 +1,26 @@
 package com.makeitbig.eventapp.security;
 
-import com.makeitbig.eventapp.model.UserRole;
+import com.makeitbig.eventapp.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final UserService userService;
+
+    @Autowired
+    public SecurityConfig(UserService userService) {
+        this.userService = userService;
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception
     {
@@ -26,5 +36,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .csrf().disable(); // disable cross site request forgery, as we don't use cookies - otherwise ALL PUT, POST, DELETE will get HTTP 403!
     }
 
+    @Override
+    public void configure(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
+        authenticationManagerBuilder
+                .userDetailsService(userService)
+                .passwordEncoder(passwordEncoder());
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
 }
